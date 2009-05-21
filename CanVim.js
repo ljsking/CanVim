61ef21a5c397eb5f182d65ca0001edc4a1b19feb
Jaesung Lee <ljsking@gmail.com>
2009-05-21 17:06:49 +0900

    If user press enter key, CanVim treats it well. New line object is created and added into text array. Draw function fills text with new lines.

--- CanVim.js
+++ CanVim.js
@@ -1,11 +1,22 @@
-var text = 'Sample String';
+var text=new Array();
+text.push('This is text');
+text.push('isn\' it?');
+var currentLine = 1;
+var debug = ''
 
 function draw() {
     var canvas = document.getElementById("canvas");
     if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
-ctx.clearRect(0, 0, 300, 300)
-		ctx.fillText(text, 10, 50);  
+		ctx.clearRect(0, 0, 300, 300);
+		//text.length
+		var x = 5;
+		var y = 10;
+		for(var index in text){
+			ctx.fillText(text[index], x, y);
+			y+=15
+		}
+		ctx.fillText(debug, x, y);
     }
 }
 
@@ -13,12 +24,28 @@ var KEY = {
     RIGHT: 39,
     UP: 38,
     LEFT: 37,
-    DOWN: 40
+    DOWN: 40,
+	ALT: 91,
+	ESC: 27,
+	ENTER: 13
 };
 
 function press(evt) {
     var code = evt.keyCode;
 	var keychar = String.fromCharCode(code);
-	text+=keychar;
+	switch(code){
+		case KEY.ENTER:
+			text.push('');
+			currentLine++;
+			debug = 'keyCode: Enter';
+			break;
+		case KEY.ALT:
+			debug = 'keyCode: Alt';
+			break;
+		default:
+			text[currentLine]+=keychar;
+			debug = 'keyCode: '+code;
+			break;
+	}
     draw();
 }
